ca3a4c5bb181cf55fa625593385c784e07c1fc08
RobinEstre <restrella@iprime.pe>
2024-05-30 10:39:42 -0500

    sa

--- src/app/shared/layout/sidebar/sidebar.component.ts
+++ src/app/shared/layout/sidebar/sidebar.component.ts
@@ -15,7 +15,7 @@ export class SidebarComponent implements OnInit {
     private navBarService: NavbarService,) { this.url=document.location.href.split('/')}
   userName:any;userImg:any; perfil:any; menuItems: any; rol:any;url:any
   secretrol = 'K56QSxGeKImwBRmiY';
-  rus = localStorage.getItem('rus');
+  rus = localStorage.getItem('rus');user_rol:any;
 
   ngOnInit(): void {
     this.listMenu();
@@ -40,6 +40,7 @@ export class SidebarComponent implements OnInit {
     let rol= this.rol
     switch (rol) {
       case 'is_student':
+        this.user_rol='alumno'
         this.navBarService.getMenuToStudent().subscribe(menuItems => {
           this.menuItems = menuItems['menu'];
           // this.router.events.subscribe((event) => {
@@ -66,6 +67,7 @@ export class SidebarComponent implements OnInit {
         });
         break;
       case 'is_teacher':
+        this.user_rol='profesor'
         this.navBarService.getMenuToTeacher().subscribe(navteacher =>{
           if (navteacher['success'] === 'true') {
             this.menuItems = navteacher['menu'];
@@ -94,6 +96,7 @@ export class SidebarComponent implements OnInit {
         }, error => {});
         break;
       case 'is_academic':
+        this.user_rol='academico'
         this.navBarService.getMenuToAcademic().subscribe(navacademic => {
           if (navacademic['success'] === 'true') {
             this.menuItems = navacademic['menu'];
@@ -122,6 +125,7 @@ export class SidebarComponent implements OnInit {
         }, error => {});
         break;
       case 'is_seller':
+        this.user_rol='vendedor'
         this.navBarService.getMenuToSeller().subscribe(itemnav => {
           if (itemnav['success'] === 'true') {
             this.menuItems = itemnav['menu'];
@@ -178,6 +182,7 @@ export class SidebarComponent implements OnInit {
         }, error => {});
         break;
       case 'is_cobranza':
+        this.user_rol='cobranza'
         this.navBarService.getMenuToCobranza().subscribe(itemnav => {
           if (itemnav['success'] === 'true') {
             this.menuItems = itemnav['menu'];
@@ -373,7 +378,8 @@ export class SidebarComponent implements OnInit {
           }
         }, error => {});
         break;
-        case 'is_marketing':
+      case 'is_marketing':
+        this.user_rol='marketing'
         this.navBarService.getMenuToMarketing().subscribe(itemnav => {
           if (itemnav['success'] === 'true') {
           this.menuItems = itemnav['menu'];
